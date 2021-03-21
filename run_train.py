032d21c892d00ab13dc3244593b1ccead2ed3019
i207M <i207M@outlook.com>
2021-03-21 21:16:47 +0800

    before test

--- run_train.py
+++ run_train.py
@@ -1,9 +1,9 @@
 run_str = 'python train.py --device 0 --name yolov4 \
---data ./data/light_coco.yaml --cfg ./cfg/yolov4-mine82.cfg --weights ./pth/yolov4.weights \
+--data ./data/light_coco.yaml --cfg ./cfg/yolov4-mine82.cfg --weights ./pth/24_best.weights \
 --batch-size 16 --epochs 300 --img 640 640'
 
 test_str = 'python test.py --device 0 \
---data ./data/light_coco.yaml --cfg ./cfg/yolov4-mine.cfg --weights weights/best_yolov4.pt \
+--data ./data/light_coco.yaml --cfg ./cfg/yolov4-mine82.cfg --weights weights/24_best.pt \
 --batch 8 --conf 0.001 --img 640'
 
 print(run_str)
