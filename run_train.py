92c84f7a58aebae995b71bf0af43f6e4c9cfd5ef
i207M <i207M@outlook.com>
2021-03-21 17:47:03 +0800

    error

--- run_train.py
+++ run_train.py
@@ -1,6 +1,6 @@
 run_str = 'python train.py --device 0 --name yolov4 \
 --data ./data/light_coco.yaml --cfg ./cfg/yolov4-mine.cfg --weights ./pth/yolov4.weights \
---batch-size 16 --epochs 100 --img 640 640'
+--batch-size 16 --epochs 300 --img 640 640'
 
 test_str = 'python test.py --device 0 \
 --data ./data/light_coco.yaml --cfg ./cfg/yolov4-mine.cfg --weights weights/best_yolov4.pt \
