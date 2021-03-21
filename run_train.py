50efedf15fa2235f5476bda9b2eb5b6726cee326
i207M <i207M@outlook.com>
2021-03-21 15:13:23 +0800

    prepare

--- run_train.py
+++ run_train.py
@@ -1,5 +1,5 @@
 command_str = 'python train.py --device 0 --name yolov4 \
---data data/light/coco.yaml --cfg cfg/yolov4-mine.cfg --weights pth/yolov4.weights \
+--data ./data/light_coco.yaml --cfg ./cfg/yolov4-mine.cfg --weights ./pth/yolov4.weights \
 --batch-size 16 --epochs 300 --img 640 640'
 
 print(command_str)
