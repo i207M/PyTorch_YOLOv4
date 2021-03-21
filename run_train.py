cb10716f85df80888a8bac4cd18a968f704e5472
i207M <i207M@outlook.com>
2021-03-21 17:03:11 +0800

    run done

--- run_train.py
+++ run_train.py
@@ -1,5 +1,10 @@
-command_str = 'python train.py --device 0 --name yolov4 \
+run_str = 'python train.py --device 0 --name yolov4 \
 --data ./data/light_coco.yaml --cfg ./cfg/yolov4-mine.cfg --weights ./pth/yolov4.weights \
---batch-size 16 --epochs 300 --img 640 640'
+--batch-size 16 --epochs 100 --img 640 640'
 
-print(command_str)
+test_str = 'python test.py --device 0 \
+--data ./data/light_coco.yaml --cfg ./cfg/yolov4-mine.cfg --weights weights/best_yolov4.pt \
+--batch 8 --conf 0.001 --img 640'
+
+print(run_str)
+print(test_str)
