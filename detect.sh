8f4eec530145acd6d9cb2e11c27c2f31e2ca40d2
i207M <i207M@outlook.com>
2021-03-22 15:33:00 +0800

    update

--- detect.sh
+++ detect.sh
@@ -1,3 +1,3 @@
 python detect.py --device 2 --names data/light_coco/coco.names \
---source data/light_coco/images/test.txt --cfg cfg/yolov4-mine82.cfg --weights pth/30_best.pt \
+--source data/light_coco/images/IMG_0044.MOV --cfg cfg/yolov4-mine82.cfg --weights pth/30_best.pt \
 --conf-thres 0.4 --iou-thres 0.5 --img 640 --output output/
--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 python train.py --device 0 --name yolov4_2 \
---data data/light_coco2.yaml --cfg cfg/yolov4-mine2.cfg --weights '' \
+--data data/light_coco2.yaml --cfg cfg/yolov4-mine2.cfg --weights pth/yolov4.weights \
 --batch-size 16 --epochs 300 --img 640 640
