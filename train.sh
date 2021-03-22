a2482210149b2e38a77fb276c842dee12022e216
i207M <i207M@outlook.com>
2021-03-22 11:34:00 +0800

    before 2

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
-python train.py --device 0 --name yolov4 \
---data data/light_coco.yaml --cfg cfg/yolov4-mine82.cfg --weights pth/yolov4.weights \
---batch-size 16 --epochs 400 --img 640 640
+python train.py --device 0 --name yolov4_2 \
+--data data/light_coco2.yaml --cfg cfg/yolov4-mine2.cfg --weights '' \
+--batch-size 16 --epochs 300 --img 640 640
