1ed1dce108c529d64fc6479d58a57e71cefd0750
i207M <connect@mail.i207m.top>
2021-06-02 18:23:48 +0800

    label not ok

--- auto.py
+++ auto.py
@@ -1,6 +1,5 @@
 import os
 import shutil
-import stat
 import time
 import json
 
@@ -22,7 +21,7 @@ def make_yaml(name: str, dir: str, target: str):
     return instance_json
 
 
-def make_cfg(name: str, dir: str, target: str, n_class: int) -> None:
+def make_cfg(name: str, target: str, n_class: int) -> None:
     max_bathes = n_class * 2000
     template = open(os.path.join(DATA_DIR, 'template.cfg')).read()
 
@@ -41,11 +40,37 @@ def make_sh(name: str, target: str) -> None:
         f.write('conda activate yolo-fix\n')
         f.write('cd /home/dlenv/deploy/PyTorch_YOLOv4\n')
         f.write(
-            f'python train.py --name {name}_{time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())} '
+            f'python train.py --device=1 --name {name}_{time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())} '
             f'--data {os.path.join(target,name+".yaml")} --cfg {os.path.join(target,name+".cfg")} --weights pth/yolov4.weights '
             f'--batch-size 16 --epochs 300 --img 640 640\n'
         )
     os.chmod(sh_path, 0o777)
+
+
+def make_label(target: str, instance_json) -> None:
+    label_dir = os.path.join(target, 'labels')
+    os.mkdir(label_dir)
+    imgid_to_data = {}
+    label_dict = {}
+    for dat in instance_json['images']:
+        id = dat['id']
+        label_dict[id] = []
+        imgid_to_data[id] = {'name': dat['file_name'], 'w': dat['width'], 'h': dat['height']}
+    for dat in instance_json['annotations']:
+        # WARNING: MINUS 1
+        imgid = dat['image_id']
+        hw = imgid_to_data[imgid]
+        category_id = dat['category_id'] - 1
+        bbox = dat['bbox']
+        nom_center_x = (bbox[0] + bbox[2] / 2) / hw['h']
+        nom_center_y = (bbox[1] + bbox[3] / 2) / hw['w']
+        nom_w = bbox[2] / hw['w']
+        nom_h = bbox[3] / hw['h']
+        label_dict[imgid].append(f'{category_id} {nom_center_x} {nom_center_y} {nom_w} {nom_h}\n')
+
+    for id, labels in label_dict.items():
+        with open(os.path.join(label_dir, imgid_to_data[id]['name'][:-4] + '.txt'), 'w') as f:
+            f.write(''.join(labels))
 
 
 def gen(name: str, dir: str, target: str) -> None:
@@ -53,7 +78,8 @@ def gen(name: str, dir: str, target: str) -> None:
         shutil.rmtree(target)
     shutil.copytree(dir, target)
     instance_json = make_yaml(name, dir, target)
-    make_cfg(name, dir, target, len(instance_json['categories']))
+    make_label(target, instance_json)
+    make_cfg(name, target, len(instance_json['categories']))
     make_sh(name, target)
 
 
