b029caf1c43b2aa72215fd93b15b2db5537ff3cc
i207M <connect@mail.i207m.top>
2021-06-02 16:01:36 +0800

    clear dir

--- old/test.sh
+++
@@ -1,3 +0,0 @@
-python test.py --device 1 --names data/light_coco/coco.names \
---data data/light_coco.yaml --cfg cfg/yolov4-mine82.cfg --weights pth/29_best.pt \
---batch 8 --conf-thres 0.4 --iou-thres 0.4 --img 640
